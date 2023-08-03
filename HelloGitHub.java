aa20ca52a256c9005962bc1216573828c3e73090
Parul Yadav <parul18065@iiitd.ac.in>
2023-08-03 13:55:41 +0530

    MTKDEMO-77 Update HelloGitHub.java

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -2,5 +2,6 @@ public class HelloGitHub {
     public static void main(String[] args){
         System.out.println("Hello GitHub");
         System.out.println("Jenkins CI CD");
+        System.out.println("New deployment");
     }
 }
